11c6613ad193fc95784a00df6a7dce2c56cdaca9
Ryan Mecca <rm358@duke.edu>
2023-02-19 15:44:46 -0500

    minor changes.

--- cache-server.cpp
+++ cache-server.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <cstdlib>
 #include <fstream>
+#include <stdio.h>
 using namespace std;
 
-int main(int argc, char * argv){
-  std::cout << "Hello" << std::flush;
+int main(int argc, char * argv) {
+  int count = 0;
   ofstream myfile;
-  std::cout.flush();
-  myfile.open ("/var/log/erss/proxy.log");
-  myfile << "Writing this to a file.\n";
-  myfile.close();
+  FILE * pFile;
+  ifstream f("/var/log/erss/proxy.log");
+
+  while (1) {
+    if (count % 1000 == 0) {
+      printf("count = %d\n", count);
+      fprintf(pFile, "This is the %d cycle\n", count);
+    }
+    // fprintf(stdout, "This is the %d cycle\n", count);
+    // cout << "hello" << endl;
+    count++;
+  }
+
   return EXIT_SUCCESS;
 }

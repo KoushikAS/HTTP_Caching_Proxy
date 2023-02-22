145929904e49f3be74a8a276a3f0e0e532d29c0d
Ryan Mecca <rm358@duke.edu>
2023-02-22 13:22:44 -0500

    threads

--- cache-server.cpp
+++ cache-server.cpp
@@ -7,17 +7,33 @@ Citations:
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
+#include <boost/bind/bind.hpp>
+#include <boost/thread.hpp>
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+// #include <CServerSocket.h>
 
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
 using namespace std;
+#define BOOST_BIND_GLOBAL_PLACEHOLDERS
+unordered_map<string, http::response<http::dynamic_body> > cache;
+const string HTTP_PORT = "80";
+
+string parsePath(string path, string host) {
+  path = path.erase(0, 7);              //Removing 'http://'
+  path = path.erase(0, host.length());  //Removing the host name from the url
+  return path;
+}
+
 
 boost::beast::http::response<boost::beast::http::dynamic_body> forwardRequest(
     boost::beast::http::request<boost::beast::http::string_body> & request,
     boost::asio::io_context & ioc) {
-  std::string host = std::string(request.at("Host"));
-  std::string port = "80";
+  std::string host = string(request.at("Host"));
+  std::string port = HTTP_PORT;
+  string path = parsePath(string(client_request.target()), host);
   boost::asio::ip::tcp::resolver resolver(ioc);
   boost::beast::tcp_stream stream(ioc);
 
@@ -50,6 +66,30 @@ void do_session(boost::asio::ip::tcp::socket & socket, boost::asio::io_context &
   boost::beast::http::write(socket, response);
 }
 
+boost::beast::http::response<boost::beast::http::dynamic_body> create_connection(boost::asio::ip::tcp::acceptor& acceptor, boost::asio::io_context & ioc) {
+  //Will Receive new connection
+  boost::asio::ip::tcp::socket socket{ioc};
+  
+  cout << "Waiting for connection at " << endl;
+  //Wait for the connection
+  acceptor.accept(socket);
+  // acceptor.async_accept(socket, std::bind(do_session, boost::asio::placeholders::error, socket, ioc));
+
+  boost::beast::flat_buffer buff;
+
+  boost::beast::http::request<boost::beast::http::string_body> request;
+  boost::beast::http::read(socket, buff, request);
+
+  cout << request << endl;
+
+  boost::beast::http::response<boost::beast::http::dynamic_body> response =
+      forwardRequest(request, ioc);
+  boost::beast::http::write(socket, response);
+
+  socket.shutdown(boost::asio::ip::tcp::socket::shutdown_send);
+  return response;
+}
+
 int main(int argc, char ** argv) {
   // logging
   ofstream logfile;
@@ -62,16 +102,18 @@ int main(int argc, char ** argv) {
   boost::asio::io_context ioc{1};
 
   boost::asio::ip::tcp::acceptor acceptor{ioc, {addr, port_num}};
+
   while(1) {
-    //Will Receive new connection
     boost::asio::ip::tcp::socket socket{ioc};
-    
+  
     cout << "Waiting for connection at " << endl;
     //Wait for the connection
     acceptor.accept(socket);
-
-    do_session(socket, ioc);
-    socket.shutdown(boost::asio::ip::tcp::socket::shutdown_send);
+    // std::thread{std::bind(&do_session, std::move(socket))}.detach();
+    std::thread{do_session, std::ref(socket), std::ref(ioc)}.join();
+    // boost::beast::http::response<boost::beast::http::dynamic_body> response = create_connection(acceptor, ioc);
+    // logfile << response << endl;
+    // create_connection(acceptor, ioc);
   }
   cout << "Ending the server" << endl;
   logfile.close();
